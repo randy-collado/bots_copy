8db8e8165150d77a0519eff60e7682d6ff0b5c4e
Randy Collado <randy64@bu.edu>
2020-03-22 02:14:10 -0400

    updated to input password into page

--- bot.py
+++ bot.py
@@ -6,6 +6,9 @@ class FacebookBot():
 
     def login(self):
         self.driver.get('https://facebook.com')
-        
+
         fb_login_username = self.driver.find_element_by_xpath('//*[@id="email"]')
         fb_login_username.send_keys('6462023324')
+
+        fb_login_password = self.driver.find_element_by_xpath('//*[@id="pass"]')
+        fb_login_password.send_keys('whitney1997')
